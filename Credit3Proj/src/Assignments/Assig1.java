daa217eed7dcc25e033b08e620e2dbbe8ce419cb
16084005 <16084005@L815WD220010.edu.cbe.ab.ca>
2022-04-27 13:32:41 -0600

    1:32Pm

--- Credit3Proj/src/Assignments/Assig1.java
+++ Credit3Proj/src/Assignments/Assig1.java
@@ -1,5 +1,24 @@
 package Assignments;
 
-public class Assig1 {
+public class Assig1
+{
+	
+
+	public static void main(String[] args) 
+	{
+	    System.out.println("Ice Trae ");
+	    
+	    System.out.println("From Mr:Wilson");
+	    
+	    System.out.println("Address ");
+
+	    System.out.println("Date: 4/27/2022 \n\n ");  
+	    
+	    System.out.println("Kurtis Cheng The Chosen \n\n");  
+	   
+	    System.out.println("Help I can't guard Kurtis Cheng he's too unstoppable!");  
+	    
+	    
+	}
 
 }
--- Credit3Proj/src/Exercises/Ex1.java
+++ Credit3Proj/src/Exercises/Ex1.java
@@ -1,5 +1,11 @@
 package Exercises;
 
-public class Ex1 {
+public class Ex1 
+{
 
+	public static void main(String[] args) {
+	    System.out.println("Ice Trae");
+	    
+	  }
+	
 }

23be5df9903cb7bed6438e3dd613b930c1d1d2a6
16084005 <16084005@L815WD220010.edu.cbe.ab.ca>
2022-05-04 13:27:48 -0600

    1:27

---
+++ Credit3Proj/src/Assignments/Assig2.java
@@ -0,0 +1,60 @@
+package Assignments;
+
+public class Assig2 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("*");		
+		System.out.println("*");
+		System.out.println("*       *");		
+		System.out.println("*      *");
+		System.out.println("*     *");		
+		System.out.println("*    *");
+		System.out.println("*   *");		
+		System.out.println("*  *");
+		System.out.println("* *");		
+		System.out.println("*");
+		System.out.println("* *");		
+		System.out.println("*   *");
+		System.out.println("*    *");		
+		System.out.println("*     *");
+		System.out.println("*      *");		
+		System.out.println("*       *");
+		System.out.println("*        *");
+		System.out.println("*         *");
+		
+		System.out.println("");		
+		System.out.println("");		
+		
+		
+		System.out.println("");		
+		System.out.println("");
+		System.out.println("");		
+		System.out.println("******************");
+		System.out.println("*");		
+		System.out.println("*");
+		System.out.println("*");		
+		System.out.println("*");
+		System.out.println("*");		
+		System.out.println("*");
+		System.out.println("*");		
+		System.out.println("*");
+		System.out.println("*");		
+		System.out.println("*");
+		System.out.println("*");		
+		System.out.println("*");
+		System.out.println("********************");		
+		System.out.println("*");
+		System.out.println("*");		
+		System.out.println("*");
+		System.out.println("*");		
+		System.out.println("*");
+		System.out.println("*");		
+		System.out.println("*");
+		
+		
+		
+	}
+	
+}

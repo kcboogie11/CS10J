fb29a935e44d4e819cd5049b54c7c019f713fa93
16084005 <16084005@L815WD220010.edu.cbe.ab.ca>
2022-05-19 15:06:09 -0600

    hello

---
+++ credit4/src/assignments/Assig6.java
@@ -0,0 +1,51 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class Assig6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		 Scanner userinput = new Scanner(System.in);
+	       System.out.println("What is your first name? ");
+	        String name = userinput.nextLine();
+	        
+	       System.out.println("What is your last name? ");
+	        String name2 =  userinput.nextLine();
+	        
+	       System.out.println("What grade are you in? ");
+	        String Grade =  userinput.nextLine();
+	        
+	       System.out.println("What school do you attend? ");
+	        String Schoolname = userinput.nextLine();
+	        
+	       System.out.println("What is your favourite hobby? ");
+	        String hobby = userinput.nextLine();
+	        
+	       System.out.println("Hello, your name is " +name+ " " +name2+ ", you are currently in grade " +Grade+ " at " +Schoolname + ". Your favourite hobby is " +hobby+ ".");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ credit4/src/assignments/assignment5.java
@@ -0,0 +1,30 @@
+package assignments;
+
+public class assignment5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Prob1 = 79+3*(4+82-68)-7+19;
+        System.out.println("79 + 3 *(4+ 82– 68) – 7 + 19= " + Prob1);
+        
+        int Prob2 = (179+21+10)/7+181;
+        System.out.println("(179 + 21 + 10) / 7 + 181= " + Prob2);
+        
+        int Prob3 = 10389*56*11+2246;
+        System.out.println("10389 * 56 * 11 + 2246= " + Prob3);
+        
+
+        
+        
+        
+        
+	   }
+
+	}
+
+	
+        
+        
+        
+        
+	

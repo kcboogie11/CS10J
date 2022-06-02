c6252a04de069f8e8ab56c298ea2372d2aabd1a4
16084005 <16084005@L815WD220010.edu.cbe.ab.ca>
2022-06-02 14:40:03 -0600

    Lakers 2023

---
+++ credit5/src/assigments/Assig14.java
@@ -0,0 +1,48 @@
+package assigments;
+
+import java.util.Scanner;
+
+public class Assig14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 //Ask for age to verify your stage of life
+        Scanner userinput = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        int age = userinput.nextInt();
+        //if else statements to determine what stage of life you belong due to your age
+        if (age < 5)
+        {
+        System.out.println("You are a toddler");
+        }
+       
+        else if ((age < 10)&&(age > 6))               
+        {
+        System.out.println("You are a child");
+        }
+        
+        else if ((age < 12)&&(age > 11))
+        {
+        System.out.println("You are a preteen");
+        }
+       
+        else if ((age < 17)&&(age > 13))
+        {
+        System.out.println("You are a teen");
+        }
+       
+        else
+        {
+        System.out.println("You are an adult");
+        }
+        
+	}
+
+}
+/* Screen Dump
+
+Please enter your age: 
+22
+You are an adult
+ 
+ */
